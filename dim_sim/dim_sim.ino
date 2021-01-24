d1b6800ba3acc177d78cf0f26c07fda3050fe96e
andrewgabler <handyandychp@gmail.com>
2021-01-23 19:16:58 -0600

    Auto formatted code and fixed blinker function, left, right, and both now functioning.

--- dim_sim/dim_sim.ino
+++ dim_sim/dim_sim.ino
@@ -26,17 +26,17 @@ unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned long address;
 unsigned long addrLi[10] = {0x217FFC, 0x2803008, 0x3C01428, 0x381526C, 0x3600008, 0xA10408, 0x2006428, 0x1A0600A, 0x2616CFC, 0x1017FFC};
 /*
- * addrLi[0] = Speed/KeepAlive
- * addrLi[1] = RPM/Backlights
- * addrLi[2] = Coolant/OutdoorTemp
- * addrLi[3] = Time/GasTank
- * addrLi[4] = Brakes Keep alive?
- * addrLi[5] = Blinker
- * addrLi[6] = Anti-Skid
- * addrLi[7] = Aibag Light
- * addrLi[8] = 4C Error
- * addrLi[9] = Car Config
- */
+   addrLi[0] = Speed/KeepAlive
+   addrLi[1] = RPM/Backlights
+   addrLi[2] = Coolant/OutdoorTemp
+   addrLi[3] = Time/GasTank
+   addrLi[4] = Brakes Keep alive?
+   addrLi[5] = Blinker
+   addrLi[6] = Anti-Skid
+   addrLi[7] = Aibag Light
+   addrLi[8] = 4C Error
+   addrLi[9] = Car Config
+*/
 
 //Refer to Excel Sheets for info about data values.
 unsigned char defaultData[10][8] = {
@@ -55,33 +55,33 @@ unsigned char defaultData[10][8] = {
 //Sets the data for what the car is equiped with.
 //Taken from a 2007 M66 SR with Climate package, rear parking sensors, no nav.
 unsigned char carConfigData[16][8] = {
-    {0x10, 0x10, 0x01, 0x03, 0x02, 0x01, 0x00, 0x01},
-    {0x11, 0x18, 0x05, 0x05, 0x02, 0x03, 0x01, 0x05},
-    {0x12, 0x03, 0x02, 0x02, 0x01, 0x02, 0x05, 0x01},
-    {0x13, 0x01, 0x01, 0x01, 0x02, 0x02, 0x01, 0x04},
-    {0x14, 0x04, 0x01, 0x02, 0x01, 0x02, 0x03, 0x11},
-    {0x15, 0x15, 0x03, 0x02, 0x02, 0x01, 0x01, 0x01},
-    {0x16, 0x02, 0x62, 0x02, 0x01, 0x02, 0x01, 0x02},
-    {0x17, 0x01, 0x02, 0x02, 0x01, 0x03, 0x03, 0x01},
-    {0x18, 0x01, 0x05, 0x03, 0x03, 0x12, 0x04, 0x04},
-    {0x19, 0x11, 0x02, 0x10, 0x01, 0x05, 0x01, 0x02},
-    {0x1A, 0x01, 0x02, 0x01, 0x04, 0x01, 0x01, 0x03},
-    {0x1B, 0x01, 0x02, 0x10, 0x01, 0x01, 0x01, 0x01},
-    {0x1C, 0x01, 0x04, 0x01, 0x03, 0x01, 0x06, 0x01},
-    {0x1D, 0x04, 0x02, 0x01, 0x01, 0x01, 0x01, 0x01},
-    {0x1E, 0x01, 0x03, 0x01, 0x01, 0x01, 0x01, 0x01},
-    {0x1F, 0x01, 0x02, 0x05, 0x02, 0x02, 0x22, 0x01}
+  {0x10, 0x10, 0x01, 0x03, 0x02, 0x01, 0x00, 0x01},
+  {0x11, 0x18, 0x05, 0x05, 0x02, 0x03, 0x01, 0x05},
+  {0x12, 0x03, 0x02, 0x02, 0x01, 0x02, 0x05, 0x01},
+  {0x13, 0x01, 0x01, 0x01, 0x02, 0x02, 0x01, 0x04},
+  {0x14, 0x04, 0x01, 0x02, 0x01, 0x02, 0x03, 0x11},
+  {0x15, 0x15, 0x03, 0x02, 0x02, 0x01, 0x01, 0x01},
+  {0x16, 0x02, 0x62, 0x02, 0x01, 0x02, 0x01, 0x02},
+  {0x17, 0x01, 0x02, 0x02, 0x01, 0x03, 0x03, 0x01},
+  {0x18, 0x01, 0x05, 0x03, 0x03, 0x12, 0x04, 0x04},
+  {0x19, 0x11, 0x02, 0x10, 0x01, 0x05, 0x01, 0x02},
+  {0x1A, 0x01, 0x02, 0x01, 0x04, 0x01, 0x01, 0x03},
+  {0x1B, 0x01, 0x02, 0x10, 0x01, 0x01, 0x01, 0x01},
+  {0x1C, 0x01, 0x04, 0x01, 0x03, 0x01, 0x06, 0x01},
+  {0x1D, 0x04, 0x02, 0x01, 0x01, 0x01, 0x01, 0x01},
+  {0x1E, 0x01, 0x03, 0x01, 0x01, 0x01, 0x01, 0x01},
+  {0x1F, 0x01, 0x02, 0x05, 0x02, 0x02, 0x22, 0x01}
 };
 /*
- * Time is 0-1440
- * every mumber is a minute
- * 24 hours 
- * AM - 0- 719
- * PM - 720-1440
- */
+   Time is 0-1440
+   every mumber is a minute
+   24 hours
+   AM - 0- 719
+   PM - 720-1440
+*/
 void updateTime(int inputTime)
 {
-  if(inputTime >=0 && inputTime <= 1440){
+  if (inputTime >= 0 && inputTime <= 1440) {
     int b4;
     for (int i = 0; i < 6; i++)
     {
@@ -94,21 +94,21 @@ void updateTime(int inputTime)
     defaultData[3][4] = b4;
     defaultData[3][5] = b5;
   }
-  else 
+  else
   {
     //SERIAL.println("Not a valid time");
   }
 }
 
 /*
- * input time as 2 2 didgit numbers 12, 43 = 12:43, 4, 24 = 4:24
- * AM = 1 is AM
- * Am = 0 is PM
- * returns time in updateTime format 0 - 1440
- */
+   input time as 2 2 didgit numbers 12, 43 = 12:43, 4, 24 = 4:24
+   AM = 1 is AM
+   Am = 0 is PM
+   returns time in updateTime format 0 - 1440
+*/
 int clockToDecimal(int hour, int minute, int AM)
 {
-  if((hour >=0 && hour <= 12) && (minute >=0 && minute < 60) && (AM == 1 || AM == 0)){
+  if ((hour >= 0 && hour <= 12) && (minute >= 0 && minute < 60) && (AM == 1 || AM == 0)) {
     if (AM)
     {
       if (hour == 12)
@@ -121,16 +121,16 @@ int clockToDecimal(int hour, int minute, int AM)
       }
     }
     return ((hour * 60) + minute) + 720;
-  } 
-  else 
+  }
+  else
   {
     //SERIAL.println("Not a valid time");
     return 0;
   }
 }
 /*
- * Initialization data from logs for SRS sytem
- */
+   Initialization data from logs for SRS sytem
+*/
 void initSRS()
 {
   unsigned char temp[8] = {0xC0, 0x0, 0x0, 0x0, 0x0, 0xBC, 0xDB, 0x80};
@@ -149,10 +149,10 @@ void initSRS()
 }
 
 /*
- * Generates a value for the SRS message.
- * Couldn't detect a pattern from my logs on the meaning of the first byte.
- * First byte is decided semi-randomly and is selected based on occureance in the logs.
- */
+   Generates a value for the SRS message.
+   Couldn't detect a pattern from my logs on the meaning of the first byte.
+   First byte is decided semi-randomly and is selected based on occureance in the logs.
+*/
 void genSRS(long address, byte stmp[])
 {
   int randomNum = random(0, 794);
@@ -177,8 +177,8 @@ void genSRS(long address, byte stmp[])
 }
 
 /*
- * Init 4C message
- */
+   Init 4C message
+*/
 void init4C()
 {
   unsigned char temp[8] = {0x09, 042, 0x0, 0x0, 0x0, 0x50, 0x00, 0x00};
@@ -193,9 +193,9 @@ void init4C()
   CAN.sendMsgBuf(0x2616CFC, 1, 8, temp);
 }
 /*
-  * Generate the car config. May be mileage from CEM, not sure.
-  * Sets last 2 bytes randomly based on random occurance in the logs.
-  */
+    Generate the car config. May be mileage from CEM, not sure.
+    Sets last 2 bytes randomly based on random occurance in the logs.
+*/
 void genCC(long address, byte stmp[])
 {
   int randomNum = random(0, 7);
@@ -208,30 +208,30 @@ void genCC(long address, byte stmp[])
   delay(20);
 }
 /*
- * Generates random values for temp message
- */
- void genTemp(long address, byte stmp[]){
+   Generates random values for temp message
+*/
+void genTemp(long address, byte stmp[]) {
   int randomNum = random(0, 133);
   if (randomNum < 16)
   {
     stmp[0] = (char)0x00;
-  } 
-  else if(randomNum >= 16 && randomNum < 41)
+  }
+  else if (randomNum >= 16 && randomNum < 41)
   {
     stmp[0] = (char)0x40;
   }
-  else if(randomNum >=41 && randomNum < 76)
+  else if (randomNum >= 41 && randomNum < 76)
   {
     stmp[0] = (char)0xC0;
   }
-  else if(randomNum >=76 && randomNum < 133)
+  else if (randomNum >= 76 && randomNum < 133)
   {
     stmp[0] = (char)0x80;
   }
-  if(randomNum < 76){
+  if (randomNum < 76) {
     stmp[1] = (char)0x80;
-  } 
-  else 
+  }
+  else
   {
     stmp[1] = (char)0x00;
   }
@@ -239,72 +239,72 @@ void genCC(long address, byte stmp[])
   if (randomNum < 7)
   {
     stmp[2] = (char)0x11;
-  } 
-  else if(randomNum >= 7 && randomNum < 15)
+  }
+  else if (randomNum >= 7 && randomNum < 15)
   {
     stmp[2] = (char)0x71;
   }
-  else if(randomNum >=15 && randomNum < 25)
+  else if (randomNum >= 15 && randomNum < 25)
   {
     stmp[2] = (char)0x61;
   }
-  else if(randomNum >=25 && randomNum < 60)
+  else if (randomNum >= 25 && randomNum < 60)
   {
     stmp[2] = (char)0x51;
   }
-  else if(randomNum >=60&& randomNum < 105)
+  else if (randomNum >= 60 && randomNum < 105)
   {
     stmp[2] = (char)0x41;
   }
   CAN.sendMsgBuf(address, 1, 8, stmp);
   delay(20);
- }
- /*
-  * Convert Celsius to Fahrenheit
-  */
-double celsToFahr(double temp){
-  return ((temp * (9/5))+32);
 }
- /*
-  * Set the digital outdoor temperature with fahrenheit input 
-  * (Will always display on dim in fahrenheit)
-  */
-void setOutdoorTemp(int oTemp){
-  if(!(oTemp < -49 || oTemp > 176))
+/*
+   Convert Celsius to Fahrenheit
+*/
+double celsToFahr(double temp) {
+  return ((temp * (9 / 5)) + 32);
+}
+/*
+   Set the digital outdoor temperature with fahrenheit input
+   (Will always display on dim in fahrenheit)
+*/
+void setOutdoorTemp(int oTemp) {
+  if (!(oTemp < -49 || oTemp > 176))
   {
-    if(oTemp > -50 && oTemp <= 32){
+    if (oTemp > -50 && oTemp <= 32) {
       defaultData[2][4] = 0x0D;
-      defaultData[2][5] = ceil((oTemp+83)*2.21);
+      defaultData[2][5] = ceil((oTemp + 83) * 2.21);
     }
-    else if(oTemp > 32 && oTemp <= 146)
+    else if (oTemp > 32 && oTemp <= 146)
     {
       defaultData[2][4] = 0x0E;
-      defaultData[2][5] = ceil((oTemp-33)*2.25);
+      defaultData[2][5] = ceil((oTemp - 33) * 2.25);
     }
-    else if(oTemp > 146 && oTemp < 177)
+    else if (oTemp > 146 && oTemp < 177)
     {
       defaultData[2][4] = 0x0F;
-      defaultData[2][5] = ceil((oTemp-147)*2.20);
-    } 
-  } 
-  else 
+      defaultData[2][5] = ceil((oTemp - 147) * 2.20);
+    }
+  }
+  else
   {
     //SERIAL.println("Temp out of range");
   }
-  
+
 }
 /*
- * Sets the coolant gauge from low to high
- * Accepts value 0-100 low to high.
- */
-void setCoolantGauge(int range){
-  if(range >= 0 && range <= 55 ){
+   Sets the coolant gauge from low to high
+   Accepts value 0-100 low to high.
+*/
+void setCoolantGauge(int range) {
+  if (range >= 0 && range <= 55 ) {
     defaultData[2][3] = range + 88;
   }
-  else if(range > 55  && range <= 100 ){
+  else if (range > 55  && range <= 100 ) {
     //upper limit scales very slowly.
     //This calculation makes it fit the 0-100 scale much better.
-    defaultData[2][3] = ceil((range-55)*.333) + 173;
+    defaultData[2][3] = ceil((range - 55) * .333) + 173;
   }
   else
   {
@@ -312,101 +312,101 @@ void setCoolantGauge(int range){
   }
 }
 /*
- * Sets the speed in mph
- */
- void setCarSpeed(int carSpeed){
-  if(carSpeed >= 0 && carSpeed <= 160){
-    if(carSpeed >= 0 && carSpeed <= 40){
+   Sets the speed in mph
+*/
+void setCarSpeed(int carSpeed) {
+  if (carSpeed >= 0 && carSpeed <= 160) {
+    if (carSpeed >= 0 && carSpeed <= 40) {
       defaultData[0][5] = 0x58;
       defaultData[0][6] = round(carSpeed * 6.375);
     }
-    else if(carSpeed > 40 && carSpeed <= 80)
+    else if (carSpeed > 40 && carSpeed <= 80)
     {
       defaultData[0][5] = 0x59;
       defaultData[0][6] = round(carSpeed * 6.375);
     }
-    else if(carSpeed > 80 && carSpeed <= 120)
+    else if (carSpeed > 80 && carSpeed <= 120)
     {
       defaultData[0][5] = 0x5A;
       defaultData[0][6] = round(carSpeed * 6.375);
     }
-    else if(carSpeed > 120 && carSpeed <= 160)
+    else if (carSpeed > 120 && carSpeed <= 160)
     {
       defaultData[0][5] = 0x5B;
       defaultData[0][6] = round(carSpeed * 6.375);
     }
-  } 
-  else 
+  }
+  else
   {
     //SERIAL.println("Speed out of range");
   }
- }
- /*
-  * Set gas gauge level 0-100
-  */
- void setGasLevel(int level){
-  if(level >=0 && level <= 100){
+}
+/*
+   Set gas gauge level 0-100
+*/
+void setGasLevel(int level) {
+  if (level >= 0 && level <= 100) {
     defaultData[3][6] = round(level * .64 );
     defaultData[3][7] = round(level * .64 );
-  } 
-  else 
+  }
+  else
   {
     //SERIAL.println("Gas level out of range");
   }
- }
+}
 /*
- * Set RPMs 502-8000
- * Will not match gauge precicely, but close as can be.
- * 501 and lower = 0x01 which is a no go.
- * Very weird system for rpm it counts 0-255 by increments of 32 and rolls over 
- */
-void setRpm(int rpm){
-  if(rpm > 501 && rpm <= 8000){
-    defaultData[1][6] = floor((rpm*.031875)/32) * 32  + floor((rpm*.031875)/8);
-  } 
-  else 
+   Set RPMs 502-8000
+   Will not match gauge precicely, but close as can be.
+   501 and lower = 0x01 which is a no go.
+   Very weird system for rpm it counts 0-255 by increments of 32 and rolls over
+*/
+void setRpm(int rpm) {
+  if (rpm > 501 && rpm <= 8000) {
+    defaultData[1][6] = floor((rpm * .031875) / 32) * 32  + floor((rpm * .031875) / 8);
+  }
+  else
   {
     //SERIAL.println("RPM out of range");
   }
-  
+
 }
 /*
- * Set Overhead led brightness 0-256
- */
- void setOverheadBrightness(int value){
-  if(value >= 0 && value <= 256){
+   Set Overhead led brightness 0-256
+*/
+void setOverheadBrightness(int value) {
+  if (value >= 0 && value <= 256) {
     defaultData[1][2] = value;
   }
-  else 
+  else
   {
     //SERIAL.println("Value out of range");
   }
- }
- /*
- * Set LCD brightness 0-256
- */
- void setLcdBrightness(int value){
-  if(value >= 0 && value <= 256){
-    defaultData[1][4] = round(value/32);
+}
+/*
+  Set LCD brightness 0-256
+*/
+void setLcdBrightness(int value) {
+  if (value >= 0 && value <= 256) {
+    defaultData[1][4] = round(value / 32);
   }
-  else 
+  else
   {
     //SERIAL.println("Value out of range");
   }
- }
- /*
- * Set brightness of all lights
- */
- void setTotalBrightness(int value){
-  if(value >= 0 && value <= 256){
+}
+/*
+  Set brightness of all lights
+*/
+void setTotalBrightness(int value) {
+  if (value >= 0 && value <= 256) {
     setLcdBrightness(value);
     setOverheadBrightness(value);
   }
-  else 
+  else
   {
     //SERIAL.println("Value out of range");
   }
- }
+}
 /*
    Set left blinker to ON or OFF
 */
@@ -444,28 +444,38 @@ void genBlinking(long address, byte stmp[], bool isBlinking, int interval, int b
   }
   if (isBlinking && (interval % blinkRatio == 0)) {
     int prevState = 0;
-    if(defaultData[5][7] == 0x0A || defaultData[5][7] == 0x0C){
-      prevState = 1;
-    }
-    //currently just leaves both on
-    if (leftBlinker) {
-      if (prevState == 1) {
-        defaultData[5][7] = 0x0E;
-      } else {
-        defaultData[5][7] = 0x10;
-      }
-      stmp[7] = defaultData[5][7];
-      CAN.sendMsgBuf(address, 1, 8, stmp);
-    }
-    //currently properly blinks the right blinker
-    if (rightBlinker) {
-      if (prevState == 1) {
+    if (leftBlinker && rightBlinker) {
+      if (defaultData[5][7] == 0x0E) {
         defaultData[5][7] = 0x08;
       } else {
-        defaultData[5][7] = 0x0C;
+        defaultData[5][7] = 0x0E;
       }
       stmp[7] = defaultData[5][7];
       CAN.sendMsgBuf(address, 1, 8, stmp);
+    } else {
+      if (defaultData[5][7] == 0x0A || defaultData[5][7] == 0x0C) {
+        prevState = 1;
+      }
+      //blinks left blinker
+      if (leftBlinker) {
+        if (prevState == 1) {
+          defaultData[5][7] = 0x08;
+        } else {
+          defaultData[5][7] = 0x0A;
+        }
+        stmp[7] = defaultData[5][7];
+        CAN.sendMsgBuf(address, 1, 8, stmp);
+      }
+      //blinks right blinker
+      if (rightBlinker) {
+        if (prevState == 1) {
+          defaultData[5][7] = 0x08;
+        } else {
+          defaultData[5][7] = 0x0C;
+        }
+        stmp[7] = defaultData[5][7];
+        CAN.sendMsgBuf(address, 1, 8, stmp);
+      }
     }
   }
 }
@@ -498,12 +508,13 @@ void setup()
   setGasLevel(90);
   setRpm(3400);
   setTotalBrightness(256);
-  setLeftBlinker(true);
+  //setLeftBlinker(true);
+  setRightBlinker(true);
 }
 /*
- * Main message organization 
- */
-void simDim(){
+   Main message organization
+*/
+void simDim() {
   address = addrLi[cnt];
   for (int i = 0; i < 8; i++)
   {
@@ -513,7 +524,7 @@ void simDim(){
   {
     genSRS(address, stmp);
   }
-  else if(address == 0x3C01428)
+  else if (address == 0x3C01428)
   {
     genTemp(address, stmp);
   }
